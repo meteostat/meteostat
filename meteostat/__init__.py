4743e7ab15b18eeea1ce36b33a8a5f37548cd4b1
clampr <christian.lamprecht@aol.de>
2026-03-21 11:44:31 +0100

    chore: bump version to 2.1.4

--- meteostat/__init__.py
+++ meteostat/__init__.py
@@ -12,7 +12,7 @@ The code is licensed under the MIT license.
 """
 
 __appname__ = "meteostat"
-__version__ = "2.1.3"
+__version__ = "2.1.4"
 
 from meteostat.api.daily import daily
 from meteostat.api.hourly import hourly
